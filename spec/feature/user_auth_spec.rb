68fa2cba830cead23ad6c59d8ac21c9ab4648b16
Shaneil Clarke <shaneilske@yahoo.com>
2019-02-09 14:50:19 +0000

    refactor user authentication spec

--- spec/feature/user_auth_spec.rb
+++ spec/feature/user_auth_spec.rb
@@ -1,20 +1,13 @@
 RSpec.feature 'Visits homepage', type: :feature do
   scenario 'User signs up' do
-    visit '/'
-    expect(page).to have_content('InstaPix')
     user_signup
-    click_button("Create account")
     expect(page).to have_content('Sign up successful')
     expect(page.current_path).to eq('/posts')
   end
   
   scenario 'User can sign in' do
-    visit '/'
     user_signup
-    click_button("Create account")
-    visit '/'
     user_signin
-    click_button 'Sign In'
     expect(page.current_path).to eq('/posts')
   end
 
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 def user_signup
+  visit '/'
   fill_in :user_email, with: 'socks@yahoo.com'
   fill_in :user_password, with: 'testing123'
   fill_in :user_password_confirmation, with: 'testing123'
+  click_button("Create account")
 end
 
 def user_signin
+  visit '/'
   fill_in :sign_in_email, with: 'socks@yahoo.com'
   fill_in :sign_in_password, with: 'testing123'
+  click_button 'Sign In'
 end
 
 def incorrect_signin
+  visit '/'
   fill_in :sign_in_email, with: 'socks@yahoo.com'
   fill_in :sign_in_password, with: 'testing'
+  click_button 'Sign In'
 end

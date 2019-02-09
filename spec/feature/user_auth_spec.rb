0c3113640bf047e79f4e83bd131c460ec0525ae3
Shaneil Clarke <shaneilske@yahoo.com>
2019-02-09 14:45:30 +0000

    refactored user authentication spec and code with partials

--- spec/feature/user_auth_spec.rb
+++ spec/feature/user_auth_spec.rb
@@ -2,9 +2,7 @@ RSpec.feature 'Visits homepage', type: :feature do
   scenario 'User signs up' do
     visit '/'
     expect(page).to have_content('InstaPix')
-    fill_in :user_email, with: 'socks@yahoo.com'
-    fill_in :user_password, with: 'testing123'
-    fill_in :user_password_confirmation, with: 'testing123'
+    user_signup
     click_button("Create account")
     expect(page).to have_content('Sign up successful')
     expect(page.current_path).to eq('/posts')
@@ -12,14 +10,12 @@ RSpec.feature 'Visits homepage', type: :feature do
   
   scenario 'User can sign in' do
     visit '/'
-    fill_in :user_email, with: 's@email.com'
-    fill_in :user_password, with: 'Bananas'
-    fill_in :user_password_confirmation, with: 'Bananas'
+    user_signup
     click_button("Create account")
     visit '/'
-    fill_in :sign_in_email, with: 's@email.com'
-    fill_in :sign_in_password, with: 'Bananas'
+    user_signin
     click_button 'Sign In'
     expect(page.current_path).to eq('/posts')
   end
+
 end
